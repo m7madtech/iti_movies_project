629579d50ed2569392e1f0fc04610402c6c477e5
M.Khaled <m7mad.tech@gmail.com>
2022-08-26 18:17:40 +0200

    first directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Metadata on over 45,000 movies. 26 million ratings from over 270,000 users.',
+    author='M.Khaled',
+    license='',
+)
